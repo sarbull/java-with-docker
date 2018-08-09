60d524c31b692afe105b649c2f1cbdc5cea6eddb
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-08-09 14:39:02 +0300

    First commit

---
+++ src/main/java/com/pidgeon/wings/WingsApplication.java
@@ -0,0 +1,12 @@
+package com.pidgeon.wings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WingsApplication.class, args);
+	}
+}
