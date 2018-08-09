e7c50bbba602f9d0a9289f9373a0ff7848a36d96
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-08-09 14:41:47 +0300

    Reindent

--- src/main/java/com/pidgeon/wings/WingsApplication.java
+++ src/main/java/com/pidgeon/wings/WingsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WingsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WingsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(WingsApplication.class, args);
+  }
 }
